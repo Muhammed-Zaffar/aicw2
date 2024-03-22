711c1a411ffb09a7cbdda08fcb058cab21ece2ea
Muhammed-Zaffar <m.kaduji-22@student.lboro.ac.uk>
2024-03-22 04:17:36 +0000

    forgot to commit main

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+import os
+import time
+
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib
 import warnings
 
 # Suppress DeprecationWarning
@@ -12,8 +16,18 @@ def sigmoid(x):
 
 
 class NeuralNetwork:
-	def __init__(self, num_input_nodes=8, num_hidden_nodes=4, num_output_nodes=1, momentum=False):
+	def __init__(self, num_input_nodes=8, num_hidden_nodes=4, num_output_nodes=1, learning_rate=0.01, momentum=False,
+				 bold_driver=False):
+
+		self.original_learning_rate = learning_rate
+		self.original_num_hidden_nodes = num_hidden_nodes
+		self.original_momentum = momentum
+		self.original_bold_driver = bold_driver
+
 		self.momentum = momentum
+		self.bold_driver = bold_driver
+
+		self.learning_rate = learning_rate  # learning rate
 		self.num_input_nodes = num_input_nodes
 		self.num_hidden_nodes = num_hidden_nodes
 		self.num_output_nodes = num_output_nodes
@@ -29,6 +43,7 @@ class NeuralNetwork:
 													  (num_hidden_nodes, num_input_nodes + 1))
 
 		self.hidden_layer = list()  # create a list to hold the output of the nodes in the hidden layer
+		self.final_activation = 0
 
 		# create the output layer weights and add a bias weight
 		self.output_nodes_weights = np.random.uniform(-2 / num_hidden_nodes, 2 / num_hidden_nodes,
@@ -64,20 +79,20 @@ class NeuralNetwork:
 			weighted_sums = np.dot(self.output_nodes_weights, self.hidden_layer[-1])
 
 			# apply the activation function to the weighted sum of the output layer
-			final_output = sigmoid(weighted_sums)
+			self.final_activation = sigmoid(weighted_sums)
 
 			# add the final output to the network
-			self.predicted_results.append(final_output)
+			self.predicted_results.append(self.final_activation)
 
 			# calculate the root mean squared error
-			error = np.sqrt(np.mean((self.data.values[i][-1] - final_output) ** 2))
+			error = np.sqrt(np.mean((self.data.values[i][-1] - self.final_activation) ** 2))
 			array.append(error)
 
 		mean = np.mean(array)
 		self.rmse.append(mean)
 		return mean
 
-	def back_propagation(self, learning_rate=0.01):
+	def back_propagation(self):
 		def sigmoid_prime(x):
 			return x * (1 - x)
 
@@ -103,26 +118,27 @@ class NeuralNetwork:
 			# Update weights from hidden to output layer
 			for j in range(self.num_hidden_nodes + 1):  # +1 for the bias weight
 				# Update the weights for the output layer; assumes single output node
-				self.output_nodes_weights[0, j] += learning_rate * output_layer_delta * self.hidden_layer[i][
-					j] + 0.9 * self.prev_update_output
+				# self.output_nodes_weights[0, j] += self.learning_rate * output_layer_delta * self.hidden_layer[i][j] + 0.9 * self.prev_update_output
+				self.output_nodes_weights[0, j] += self.learning_rate * output_layer_delta * self.final_activation + 0.9 * self.prev_update_output
 				if self.momentum:
-					self.prev_update_output = learning_rate * output_layer_delta * self.hidden_layer[i][j]
+					self.prev_update_output = self.learning_rate * output_layer_delta * self.final_activation
 
 			# Update weights from input to hidden layer
 			input_layer = np.insert(self.data.values[i][:self.num_input_nodes], 0, 1)  # Insert bias to the input layer
+			sig_ws = sigmoid(np.dot(self.hidden_nodes_weights, input_layer))
 			for h in range(self.num_hidden_nodes):
-				for j in range(self.num_input_nodes + 1):  # +1 for the bias
+				for j in range(self.num_input_nodes):  # +1 for the bias
 					# Update the weights for the hidden layer
-					self.hidden_nodes_weights[h, j] += learning_rate * hidden_layer_delta[h] * input_layer[
-						j] + 0.9 * self.prev_update_hidden
+					# print(f"{h=}, {j=}, {sig_ws=}")
+					self.hidden_nodes_weights[h, j] += self.learning_rate * hidden_layer_delta[h] * sig_ws[h] + 0.9 * self.prev_update_hidden
 					if self.momentum:
-						self.prev_update_hidden = learning_rate * hidden_layer_delta[h] * input_layer[j]
+						self.prev_update_hidden = self.learning_rate * hidden_layer_delta[h] * sig_ws[h]
 
 	def train_network(self, num_epochs=1000):
 		validation_RMSE_array = list()
+		print(f'{self.data.head()=}')
+		print(f'{pd.read_csv("files/validate.csv").head()=}')
 		for i in range(num_epochs):
-			if i % 100 == 0:
-				print(f"Training for {i} epochs")
 			self.predicted_results.clear()
 			self.forward_pass()
 			self.back_propagation()
@@ -133,49 +149,116 @@ class NeuralNetwork:
 			self.rmse.pop()
 			validation_RMSE_array.append(x)
 
+			# update the learning rate
+			if self.bold_driver:
+				if i % 1 == 0 and i != 0:
+					if validation_RMSE_array[-1] > validation_RMSE_array[-2]:
+						self.learning_rate *= 0.7
+					else:
+						self.learning_rate *= 1.05
+
+					# if the learning rate is too small, set it to 0.01
+					if self.learning_rate < 0.001:
+						self.learning_rate = 0.001
+					elif self.learning_rate > 0.2:  # if the learning rate is too large, set it to 0.15
+						self.learning_rate = 0.2
+
+			if i % 1 == 0:
+				print(f"Training for {i} epochs, lr = {self.learning_rate},training MSE = {self.rmse[-1]},validation MSE = {validation_RMSE_array[-1]}")
+
 		# print(f"{validation_RMSE_array=}")
 		# print(f"{self.rmse=}")
 
-		# self.plot_rmse(validation_RMSE_array)
-
+		self.plot_rmse(validation_RMSE_array)
 		return self.rmse
-
-	def plot_rmse(self, validation_rmse_array):
-		plt.plot(self.rmse, label="Train")
-		plt.plot(validation_rmse_array, label="Validation")
-		plt.xlabel("Epochs")
-		plt.ylabel("Root Mean Squared Error")
-		plt.title("Root Mean Squared Error vs. Epochs")
-		plt.legend()
-		plt.show()
 
 	def test_network(self):
 		# print(f"{self.hidden_nodes_weights=}")
 		# print(f"{self.output_nodes_weights=}")
 		self.data = pd.read_csv("files/test.csv")
+		self.predicted_results.clear()
 		x = self.forward_pass()
 		self.rmse.pop()
 		print(f"{x=}")
+		self.plot_predicted_results()
+
 	# self.plot_rmse()
+
+	def plot_rmse(self, validation_rmse_array):
+		global graph_counter
+		plt.plot(self.rmse, label="Train")
+		plt.plot(validation_rmse_array, label="Validation")
+		plt.xlabel("Epochs")
+		plt.ylabel("Root Mean Squared Error")
+		plt.title(f"Root Mean Squared Error vs. Epochs, \n{self.original_momentum=}, {self.original_bold_driver=}, \n{self.original_learning_rate=}, {self.original_num_hidden_nodes=}")
+		plt.plot(self.rmse.index(min(self.rmse)), min(self.rmse), "ro", label="Minimum RMSE")
+		plt.legend()
+		plt.savefig(os.path.join(graphs_dir, f"graph{graph_counter}.png"))
+		graph_counter += 1
+		plt.close()
+		# plt.show()
+
+	def plot_predicted_results(self):
+		global graph_counter
+		actual_results = [row[-1] for row in self.data.values]
+		plt.scatter(self.predicted_results, actual_results)
+		plt.xlabel("Predicted Result")
+		plt.ylabel("Actual Result")
+		plt.title(f"Predicted Results vs. Actual Results, \n{self.original_momentum=}, {self.original_bold_driver=}, \n{self.original_learning_rate=}, {self.original_num_hidden_nodes=}")
+
+		# Calculate and plot the line of best fit
+		slope, intercept = np.polyfit(actual_results, self.predicted_results, 1)
+		best_fit_line = np.array(actual_results) * slope + intercept
+		plt.plot(actual_results, best_fit_line, color='red', label='Line of Best Fit')
+
+		# Plot the line y = x to show where perfect predictions would lie
+		plt.plot([min(actual_results), max(actual_results)], [min(actual_results), max(actual_results)], color='black',
+					linestyle='--', label='Perfect Predictions')
+
+		plt.legend()
+		plt.savefig(os.path.join(graphs_dir, f"graph{graph_counter}.png"))
+		graph_counter += 1
+		plt.close()
+		# plt.show()
 
 
 if __name__ == "__main__":
-	arr = list()
-	# for i in range(4, 17):
-	nn = NeuralNetwork(num_hidden_nodes=8, momentum=False)
+	matplotlib.use('TkAgg')
+	plt.rcParams['figure.figsize'] = [8, 6]
+	manager = plt.get_current_fig_manager()
+	manager.window.wm_geometry("+0+0")
 
-	# Assume you have a method to train the network
-	nn.train_network(num_epochs=1000)
-	nn.test_network()
-	arr.append(nn.get_rmse())
+	arr = list()
+	t_start = time.perf_counter()
+	for i in range(4, 17):
+		nn = NeuralNetwork(num_hidden_nodes=i, momentum=False, bold_driver=False, learning_rate=0.1)
+
+		graph_counter = 0
+		graphs_dir = f"pics/{nn.original_num_hidden_nodes=}-{nn.original_momentum=}-{nn.original_bold_driver=}"
+		if not os.path.exists(graphs_dir):
+			os.makedirs(graphs_dir)
+
+		nn.train_network(num_epochs=1000)
+		nn.test_network()
+
+		arr.append(nn.get_rmse())
+	# end of for loop
+
 
 	for i in arr:
 		plt.plot(i, label=f"Hidden Nodes: {arr.index(i) + 4}")
 	plt.xlabel("Epochs")
 	plt.ylabel("Root Mean Squared Error")
-	plt.title("Root Mean Squared Error vs. Epochs")
+	plt.title(f"Root Mean Squared Error vs. Epochs, \n{nn.original_momentum=}, {nn.original_bold_driver=}, \n{nn.original_learning_rate=}")
+	plt.plot(nn.rmse.index(min(nn.rmse)), min(nn.rmse), "ro", label="Minimum RMSE")
 	plt.legend()
-	plt.show()
+	plt.savefig(os.path.join(f"pics/graph{graph_counter}.png"))
+	graph_counter += 1
+	plt.close()
+	# plt.show()
+
+	t_end = time.perf_counter()
+	print(f"Time taken: {t_end - t_start} seconds")
 
 # for i in arr:
 # 	final_rmse = i[-1]
